1b5f7ac0e98fcc305983edb9fac207276c36740a
Krisztina Andreyka <andreyka.kriszti@gmail.com>
2026-04-23 12:49:27 +0200

    add base

---
+++ tests/pom/homePage.ts
@@ -0,0 +1,9 @@
+export default {
+    header: {
+        searchInput: '#product-search',
+        searchButton: 'Search',
+        aboutUsButton: 'About Us',
+        loginButton: 'Log in',
+        signupButton: 'Sign up',
+    }
+}
